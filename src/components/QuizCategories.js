82b5f265a5ffff80cfad84c7abe28e2041f6ba38
dumstudent76 <64712150+dumstudent76@users.noreply.github.com>
2021-04-11 17:30:23 -0700

    Added additional categories

--- src/components/QuizCategories.js
+++ src/components/QuizCategories.js
@@ -8,18 +8,16 @@ export default function QuizCategories({ setChoice }) {
     <div className="container--categories">
       <h1>Pick Your Quiz Category!</h1>
       <div>
-        <button className="btn" onClick={() => setChoice(18)}>
-          Computer
-        </button>
-        <button className="btn" onClick={() => setChoice(9)}>
-          General
-        </button>
-        <button className="btn" onClick={() => setChoice(23)}>
-          History
-        </button>
-        <button className="btn" onClick={() => setChoice(12)}>
-          Music
-        </button>
+        <button className="btn" onClick={() => setChoice(18)}>Computer</button>
+        <button className="btn" onClick={() => setChoice(9)}>General</button>
+        <button className="btn" onClick={() => setChoice(23)}>History</button>
+        <button className="btn" onClick={() => setChoice(12)}>Music</button>
+        <button className="btn" onClick={() => setChoice(32)}>Cartoons and Animations</button>
+        <button className="btn" onClick={() => setChoice(20)}>Mythology</button>
+        <button className="btn" onClick={() => setChoice(21)}>Sports</button>
+        <button className="btn" onClick={() => setChoice(22)}>Geography</button>
+        <button className="btn" onClick={() => setChoice(10)}>Books</button>
+        <button className="btn" onClick={() => setChoice(11)}>Movies</button>
       </div>
       <div className="backButton">
       <Link to="/" className="btn">
